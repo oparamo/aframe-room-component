d7a2ba8d7e09ed7f3f42dc8b5c78c36c12643475
Oscar Paramo <oscar.paramo@me.com>
2023-02-20 14:00:14 -0600

    add doorlink lifecycle functions

--- src/components/doorlink.js
+++ src/components/doorlink.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const SCENE = 'a-scene';
+const WALL = 'a-wall';
+
 module.exports.Component = AFRAME.registerComponent('doorlink', {
   schema: {
     from: { type: 'selector' },
@@ -8,10 +11,22 @@ module.exports.Component = AFRAME.registerComponent('doorlink', {
     width: { type: 'number', default: 0.8 }
   },
   init: function () {
-    console.info('initializing doorlink');
+    const doorlinkEl = this.el;
+
+    doorlinkEl.ceiling = doorlinkEl.querySelector('a-ceiling');
+    doorlinkEl.floor = doorlinkEl.querySelector('a-floor');
+    doorlinkEl.sides = doorlinkEl.querySelector('a-sides');
+
+
+    doorlinkEl.sceneEl.systems?.building?.registerDoorlink(doorlinkEl);
   },
   update: function () {
-    console.info('updating doorlink');
+    const parentName = this.el.parentEl?.localName;
+    if (parentName !== SCENE && parentName !== WALL) {
+      const message = 'a-doorlink elements must have an "a-scene" or "a-wall" parent';
+      throw new Error(message);
+    }
+
     this.el.sceneEl.systems?.building?.examineBuilding();
   }
 });
