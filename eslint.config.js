bc49582a5b4edb6d87fe97e4c57cf082dc03d365
Oscar Paramo <oscar.paramo@me.com>
2026-04-19 12:45:13 -0500

    build: replace semistandard with ESLint 9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import neostandard from 'neostandard';
+
+export default [
+  { ignores: ['dist/**'] },
+  ...neostandard({ semi: true }),
+  {
+    languageOptions: {
+      globals: {
+        AFRAME: 'readonly',
+        THREE: 'readonly'
+      }
+    }
+  }
+];
