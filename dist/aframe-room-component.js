3ff00c0f0ade9488b311ceee3541b620d661f083
Oscar Paramo <oscar.paramo@me.com>
2026-05-01 00:05:15 -0500

    chore: build new dist files

--- dist/aframe-room-component.js
+++ dist/aframe-room-component.js
@@ -115,11 +115,17 @@
 	AFRAME.registerComponent("opening", { init: function() {
 		requireParent(this.el, "a-wall");
 		this.el.vertices = [];
+		this.el._portalEl = void 0;
 		this.el.getPortal = () => {
-			for (const dl of this.el.sceneEl.querySelectorAll("a-portal")) {
-				const data = dl.components?.portal?.data;
-				if (data?.from === this.el || data?.to === this.el) return dl;
+			if (this.el._portalEl !== void 0) return this.el._portalEl;
+			for (const portal of this.el.sceneEl.querySelectorAll("a-portal")) {
+				const data = portal.components?.portal?.data;
+				if (data?.from === this.el || data?.to === this.el) {
+					this.el._portalEl = portal;
+					return portal;
+				}
 			}
+			this.el._portalEl = null;
 			return null;
 		};
 	} });
